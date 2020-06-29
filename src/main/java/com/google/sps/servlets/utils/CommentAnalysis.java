1716959277f35c891f71aa71448975e7fca34e0d
Xin <xh23@rice.edu>
2020-06-29 14:43:26 -0500

    changed based on Rick's comments

--- src/main/java/com/google/sps/servlets/utils/CommentAnalysis.java
+++ src/main/java/com/google/sps/servlets/utils/CommentAnalysis.java
@@ -21,15 +21,12 @@ import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * This class encapsulates each element in json comment array into separate user comment object
  * and does sentiment analysis on each of them.
  */
 public class CommentAnalysis {
-  private CommentThreadListResponse youtubeResponse;
   private LanguageServiceClient languageService;
 
   public CommentAnalysis() throws IOException {
@@ -44,22 +41,18 @@ public class CommentAnalysis {
     ArrayList<Double> scoreValues = new ArrayList<>();
     for (CommentThread commentThread: youtubeResponse.getItems()) {
       UserComment userComment = new UserComment(commentThread);
-      try {
-        scoreValues.add(calculateSentiAnalysisScore(userComment));
-      } catch (IOException e) {
-        System.err.println(e.getLocalizedMessage());
-      }
+      updateSentiAnalysisScore(userComment);
+      scoreValues.add(userComment.getSentimentScore());
     }
-    System.out.println(scoreValues);
     return new Statistics(scoreValues);
   }
 
   /**
    * Perform sentiment analysis of comment.
    * @return sentiment score
-   * @throws IOException if the sentiment analysis API is not working, throw the IOExeption
+   * @throws RuntimeException if the sentiment analysis API is not working, throw the IOExeption
    */
-  private double calculateSentiAnalysisScore(UserComment comment) throws IOException  {
+  private void updateSentiAnalysisScore(UserComment comment) {
     // Start Sentiment Analysis Service.
     Document doc = Document.newBuilder().setContent(comment.getCommentMsg())
                        .setType(Document.Type.PLAIN_TEXT).build();
@@ -67,10 +60,8 @@ public class CommentAnalysis {
     if (sentiment != null) {
       double score = (double) sentiment.getScore();
       comment.setSentimentScore(score);
-      return score;
     } else {
-      comment.setSentimentScore(null);
-      throw new IOException("Failed to build the sentiments");
+      throw new RuntimeException("Failed to build the sentiments");
     }
   }
 
