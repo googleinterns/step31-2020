11445ac1b19df39e62f5c3a3bc119efe9d065dec
UriHarding <UriHarding@google.com>
2020-07-15 14:07:40 +0000

    Attempting to get jasmine tests to work

--- src/test/Javascript/controllers/urlInputTest.js
+++ src/test/Javascript/controllers/urlInputTest.js
@@ -1,3 +1,10 @@
-describe('The application controller', function() {
+var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
 
+jasmine.loadConfigFile('spec/support/jasmine.json');
+
+jasmine.execute();
+
+describe('The application controller', function() {
+  it("should explicitly fail", function () { fail('Forced to fail'); });
 });
