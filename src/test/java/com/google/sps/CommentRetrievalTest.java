d27d2146b44dece71b3767452d25a7ba3980ac6a
UriHarding <UriHarding@google.com>
2020-07-15 16:04:58 +0000

    Refactored tests to utilize nextPageToken

--- src/test/java/com/google/sps/CommentRetrievalTest.java
+++ src/test/java/com/google/sps/CommentRetrievalTest.java
@@ -38,23 +38,43 @@ public class CommentRetrievalTest {
 
   private final int HUNDRED = 100;
   private final int EXCESS_HUNDRED = 200;
-
+  private final int MAX_COMMENTS_PER_TOKEN = 100;
   private YouTubeCommentRetriever commentRetriever;
   // These url's no longer do anything; rather, serve as parameter for the call
   private final String POPULAR_VIDEO_URL = "dQw4w9WgXcQ";
   private final String UNPOPULAR_VIDEO_URL = "cA-arJ0T6L4";
+  private final String NEXT_PAGE_TOKEN = "Some Page Token";
 
+
+  public void setUp(int numExpectedComments) throws Exception{
+    setUp(numExpectedComments, null);
+  }
   // Simulate a CommentThreadListResponse with exactly as many comments as expected
-  public void setUp(int numExpectedComments) throws Exception {
+  public void setUp(int numExpectedComments, String nextPageToken) throws Exception {
     YouTube mockedYoutube = mock(YouTube.class, RETURNS_DEEP_STUBS);
     YouTube.CommentThreads.List mockedCommentThreadList =
         mock(YouTube.CommentThreads.List.class, RETURNS_DEEP_STUBS);
+    // This is only enough for two calls, all these tests require
+    // Return on the first call either 100 comments or the desired number.
+    // If there's no next page token, return nothing on the second call, 
+    // Otherwise return the remainder of comments
     when(mockedCommentThreadList.execute())
-        .thenReturn(new CommentThreadListResponse().setItems(
-            Collections.nCopies(numExpectedComments, new CommentThread())));
+        .thenReturn(
+            mockThreadListResponse(Math.min(numExpectedComments, MAX_COMMENTS_PER_TOKEN), nextPageToken),
+            mockThreadListResponse((nextPageToken == null) 
+                ? 0
+                : Math.min(numExpectedComments - MAX_COMMENTS_PER_TOKEN, MAX_COMMENTS_PER_TOKEN), nextPageToken));
     when(mockedYoutube.commentThreads().list(anyString()))
         .thenReturn(mockedCommentThreadList);
     commentRetriever = new YouTubeCommentRetriever(mockedYoutube);
+  }
+
+  // Creates list of comments of desired length. 
+  // Page token is needed for iteration but its contents are irrelevant.
+  private CommentThreadListResponse mockThreadListResponse(int desiredComments, String nextPageToken) {
+    return new CommentThreadListResponse().setItems(
+        Collections.nCopies(desiredComments, new CommentThread()))
+            .setNextPageToken(nextPageToken);
   }
 
   // The simplest case: extract 100 comments from a video with more than 100 comments
@@ -68,7 +88,7 @@ public class CommentRetrievalTest {
   // Extract more than 100 comments from a video
   @Test
   public void testExcessHundredComments() throws Exception {
-    setUp(EXCESS_HUNDRED);
+    setUp(EXCESS_HUNDRED, NEXT_PAGE_TOKEN);
     List<CommentThread> comments =
         commentRetriever.retrieveComments(POPULAR_VIDEO_URL, EXCESS_HUNDRED);
     Assert.assertEquals(comments.size(), EXCESS_HUNDRED);
@@ -94,7 +114,7 @@ public class CommentRetrievalTest {
   // Retrieve a specific amount of comments more than 100
   @Test
   public void retrievesSpecificNumCommentsExcessHundred() throws Exception {
-    setUp(120);
+    setUp(120, NEXT_PAGE_TOKEN);
     List<CommentThread> comments = commentRetriever.retrieveComments(POPULAR_VIDEO_URL, 120);
     Assert.assertEquals(comments.size(), 120);
   }
