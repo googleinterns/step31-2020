c87b148f1d6ad5fcd3ee2cc43dece63c6635bd5d
Xin <xh23@rice.edu>
2020-07-21 14:34:27 -0500

    fix jslint

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -30,7 +30,8 @@ async function getYouTubeComments() {
 }
 
 /**
- *  Fetches data and adds to html */ 
+ *  Fetches data and adds to html.
+ */
 async function getChart() {
   $('form').submit(async function() {
     document.getElementById('loading-img').style.display = 'block';
@@ -48,9 +49,9 @@ async function getChart() {
 }
 
 /**
- * Create a bar chart of sentiment score interval, frequency 
+ * Create a bar chart of sentiment score interval, frequency
  * and high magnitude comments
- * @param {Array<sentimentBucket>} sentimentBucketList 
+ * @param {Array<sentimentBucket>} sentimentBucketList
  */
 function getBarChart(sentimentBucketList) {
   const CommentSentimentTable = new google.visualization.DataTable();
@@ -59,10 +60,12 @@ function getBarChart(sentimentBucketList) {
   CommentSentimentTable.addColumn('number', 'CommentCount');
 
   // The json keys (ranges of scores) are sorted through their starting values
-  sentimentBucketList.forEach(sentimentBucket => {
-    var inclusiveStart = sentimentBucket.intervalRange.inclusiveStart;
-    var exclusiveEnd = sentimentBucket.intervalRange.exclusiveEnd;
-    CommentSentimentTable.addRow([inclusiveStart, inclusiveStart + ' to ' + exclusiveEnd, sentimentBucket.frequency]);  
+  sentimentBucketList.forEach((sentimentBucket) => {
+    const inclusiveStart = sentimentBucket.intervalRange.inclusiveStart;
+    const exclusiveEnd = sentimentBucket.intervalRange.exclusiveEnd;
+    CommentSentimentTable.addRow(
+        [inclusiveStart, inclusiveStart + ' to ' + exclusiveEnd,
+          sentimentBucket.frequency]);
   });
 
   const options = {
@@ -70,41 +73,55 @@ function getBarChart(sentimentBucketList) {
     'width': CHART_WIDTH,
     'height': CHART_HEIGHT,
     'bar': {groupWidth: '100'},
-    'tooltip': {isHtml: true}
+    'tooltip': {isHtml: true},
   };
 
   // Hide loading image once chart is drawn
   document.getElementById('loading-img').style.display = 'none';
 
-  CommentSentimentTable.sort({column: 0, desc: false}); 
-  var view = new google.visualization.DataView(CommentSentimentTable);
-  view.setColumns([1, 2]); 
+  CommentSentimentTable.sort({column: 0, desc: false});
+  const view = new google.visualization.DataView(CommentSentimentTable);
+  view.setColumns([1, 2]);
 
   const chart = new google.visualization.ColumnChart(
       document.getElementById('chart-container'));
-  chart.draw(view, options); 
+  chart.draw(view, options);
 }
 
 /**
  * Create a word cloud based on the number of appearance for each word
- * @param {Map<String:Integer>} wordFrequencyMap 
+ * @param {Map<String:Integer>} wordFrequencyMap Map that contains
+ *                                top popular words and its appearance
  */
 function getWordCloudChart(wordFrequencyMap) {
-  var data = [];
-  Object.keys(wordFrequencyMap).forEach(wordKey => data.push({'x': wordKey, 'value': wordFrequencyMap[wordKey]}));
+  const data = [];
+  Object.keys(wordFrequencyMap).forEach((wordKey) =>
+    data.push({'x': wordKey, 'value': wordFrequencyMap[wordKey]}));
   // Create a tag cloud chart
-  var chart = anychart.tagCloud(data);
+  const chart = anychart.tagCloud(data);
 
-  chart.title('Most Popular Comments')
+  chart.title('Most Popular Comments');
   // Set array of angles to 0, make all the words display horizontally
-  chart.angles([0])
+  chart.angles([0]);
   chart.container('word-cloud');
   chart.draw();
 };
+/**
+ * Get the comment content with top high magnitude.
+ * @param {List<UserComment>} userComments a sentiment buckets list
+ *                           with userComment, score, and maginitude
+ * @return {String} Top high comment message.
+ */
 function toTooltipString(userComments) {
-  return userComments.map(comment => userCommentAsString(comment)).join('<br>');
+  return userComments.map((comment) =>
+    userCommentAsString(comment)).join('<br>');
 }
 
+/**
+ * Convert a userComment object to html format
+ * @param {*} comment a user comment with high magnitude score
+ * @return {String} HTML format to display its message and magnitude
+ */
 function userCommentAsString(comment) {
   commentMagnitude = comment.magnitude;
   return comment.commentMsg + '<br> Magnitude Score: ' + commentMagnitude;
