12949770d109eacb387886b7f1ea3a21d1201f59
UriHarding <UriHarding@google.com>
2020-07-21 16:13:54 +0000

    Cleaning up an absolute mess

--- src/main/webapp/common_util.js
+++ src/main/webapp/common_util.js
@@ -17,18 +17,18 @@
   * Credit for RegEx method derived from top answer on: 
   * https://stackoverflow.com/questions/28735459/how-to-validate-youtube-url-in-client-side-in-text-box
  */	 
-function extractYouTubeUrl(url) {	function extractYouTubeUrl(url) {
-  var videoId = "";	  var videoId = "";
-  var idLength = 11; // Length of video Id's in YouTube videos	  var idLength = 11; // Length of video Id's in YouTube videos
-  if (url != undefined || url != '') {	  if (url != undefined || url != '') {
-    // This regular expression represents the different patterns that may occur in a YouTube URL	    // This regular expression represents the different patterns that may occur in a YouTube URL
-    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=|\?vi=|v\&)([^#\&\?]*).*/;	    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=|\?vi=|v\&)([^#\&\?]*).*/;
-    var match = url.match(regExp);	    var match = url.match(regExp);
-    // Check for proper format, focus on segment that contains videoId 	    // Check for proper format, focus on segment that contains videoId 
-    if (match && match[2].length == idLength) {	    if (match && match[2].length == idLength) {
-      videoId = match[2];	      videoId = match[2];
-    }     	    }     
-  }	  }
-  console.log(videoId);	  console.log(videoId);
-  return videoId;	  return videoId;
-}	}
+function extractYouTubeUrl(url) {	
+  var videoId = "";	  
+  var idLength = 11; // Length of video Id's in YouTube videos	
+  if (url != undefined || url != '') {	 
+    // This regular expression represents the different patterns that may occur in a YouTube URL
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=|\?vi=|v\&)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    // Check for proper format, focus on segment that contains videoId
+    if (match && match[2].length == idLength) {
+      videoId = match[2];
+    }   
+  }
+  console.log(videoId);
+  return videoId;
+}
