d6c2aaf1972839b53ab52e08cc5d4afbb73d4ea3
UriHarding <UriHarding@google.com>
2020-07-21 15:19:33 +0000

    Restoring stuff lost in merge

--- src/main/webapp/common_util.js
+++ src/main/webapp/common_util.js
@@ -13,16 +13,22 @@
 // limitations under the License.
 
 /*
- * Extracts video id from full url
- */ 
-function cleanseUrl(url) {
-  // Split web address from parameters, extract first parameter
-  // TODO: Add checks to make this work if video is not first parameter.
-  var videoId = url.split("?");
-  videoId = (videoId.length > 1) ? videoId[1].split("&")[0] : videoId[0];
-
-  // If param name present, remove it to isolate video Id.
-  videoId = videoId.replace("v=", "");
-  
-  return videoId;
-}
+ * Extracts video ID from the full URL	 * Extracts video ID from the full URL
+  * Credit for RegEx method derived from top answer on: 
+  * https://stackoverflow.com/questions/28735459/how-to-validate-youtube-url-in-client-side-in-text-box
+ */	 
+function extractYouTubeUrl(url) {	function extractYouTubeUrl(url) {
+  var videoId = "";	  var videoId = "";
+  var idLength = 11; // Length of video Id's in YouTube videos	  var idLength = 11; // Length of video Id's in YouTube videos
+  if (url != undefined || url != '') {	  if (url != undefined || url != '') {
+    // This regular expression represents the different patterns that may occur in a YouTube URL	    // This regular expression represents the different patterns that may occur in a YouTube URL
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=|\?vi=|v\&)([^#\&\?]*).*/;	    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=|\?vi=|v\&)([^#\&\?]*).*/;
+    var match = url.match(regExp);	    var match = url.match(regExp);
+    // Check for proper format, focus on segment that contains videoId 	    // Check for proper format, focus on segment that contains videoId 
+    if (match && match[2].length == idLength) {	    if (match && match[2].length == idLength) {
+      videoId = match[2];	      videoId = match[2];
+    }     	    }     
+  }	  }
+  console.log(videoId);	  console.log(videoId);
+  return videoId;	  return videoId;
+}	}
