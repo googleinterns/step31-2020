7c3aae5f48319324efffc2e6eadc8dc16607dbe3
UriHarding <UriHarding@google.com>
2020-07-21 18:03:43 +0000

    Wrote basic first test for UrlInput

--- src/test/javascript/controllers/urlInputTest.js
+++ src/test/javascript/controllers/urlInputTest.js
@@ -1,7 +1,24 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
   describe('The application controller', function() {
-    it('should clear the textfield when adding a new item', function() {
+    it('should pass', function() {
       expect(true).toBe(true);
+    });
+
+    it('takes in the simplest url form and returns the videoID', function() {
+      expect(extractYouTubeUrl("youtube.com/watch?v=dQw4w9WgXcQ")).toBe("dQw4w9WgXcQ");
     });
   });
 
