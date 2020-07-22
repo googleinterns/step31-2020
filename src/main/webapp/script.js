b5ed8367a2362be811463220a56eb77f17b39ddc
Xin <xh23@rice.edu>
2020-07-22 11:20:04 -0500

    revert script

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -14,18 +14,17 @@
 
 const CHART_WIDTH = 800;
 const CHART_HEIGHT = 400;
-const HIGHEST_SCORE = 1.0;
-const LOWEST_SCORE = -1.0;
-const INTERVAL = 0.2;
- 
-google.charts.load('current', {'packages':['corechart']});
-google.setOnLoadCallback(getChart)
- 
-async function getYouTubeComments(currUrl) {
-  const urlInput = currUrl; //document.getElementById('link-input');
-  console.log(currUrl);
-  const url = cleanseUrl(currUrl);
-  const response = await fetch("/YouTubeComments?url="+url);
+
+google.charts.load('current', {'packages': ['corechart']});
+google.setOnLoadCallback(getChart);
+
+/**
+ * Fetches comment data from video ID
+ */
+async function getYouTubeComments() {
+  const urlInput = document.getElementById('link-input');
+  const url = cleanseUrl(urlInput.value);
+  const response = await fetch('/YouTubeComments?url='+url);
   const comments = await response.json();
   return comments;
 }
@@ -33,59 +32,60 @@ async function getYouTubeComments(currUrl) {
 /**
  * Fetches data and adds to html
  */
-async function getChart(url) {
-  $('.button').click(async function() {
-    // After video is selected, hide the search results and display the loading icon
-    document.getElementById("video-results").style.display = "none";
-    document.getElementById('loading-img').style.display = "block";
-
-    commentStats = await getYouTubeComments(url);
-    console.log(commentStats);
+async function getChart() {
+  $('form').submit(async function() {
+    document.getElementById('loading-img').style.display = 'block';
+    commentStats = await getYouTubeComments();
     averageScore = commentStats.averageScore;
     averageMagnitude = commentStats.averageMagnitude;
-    sentimentBucketList = commentStats.sentimentBucketList; 
+    sentimentBucketList = commentStats.sentimentBucketList;
 
     const CommentSentimentTable = new google.visualization.DataTable();
-    CommentSentimentTable.addColumn('string', 'Sentiment Range');
-    CommentSentimentTable.addColumn('number', 'Comment Count');
-    CommentSentimentTable.addColumn({type: 'string', role:'tooltip', 'p': {'html': true}});
- 
-    for(i = 0; i < sentimentBucketList.length; i++) {
+    CommentSentimentTable.addColumn('number', 'InclusiveStart');
+    CommentSentimentTable.addColumn('string', 'SentimentRange');
+    CommentSentimentTable.addColumn('number', 'CommentCount');
+
+    for (i = 0; i < sentimentBucketList.length; i++) {
       currentSentimentBucket = sentimentBucketList[i];
-      rangeAsString = convertRangeToString(currentSentimentBucket.intervalRange);
+      rangeAsString = convertRangeToString(
+          currentSentimentBucket.intervalRange);
       highestMagnitudeComments = currentSentimentBucket.topNComments;
 
-      CommentSentimentTable.addRow([rangeAsString, currentSentimentBucket.frequency, 
-        toTooltipString(highestMagnitudeComments)]);
+      CommentSentimentTable.addRow([rangeAsString,
+          currentSentimentBucket.frequency,
+          toTooltipString(highestMagnitudeComments)]);
     }
- 
+
     const options = {
       'title': 'Comment Sentiment Range',
       'width': CHART_WIDTH,
-      'height':CHART_HEIGHT,
-      'bar': {groupWidth: "100"},
+      'height': CHART_HEIGHT,
+      'bar': {groupWidth: '100'},
       'tooltip': {isHtml: true}
     };
 
-    var view = new google.visualization.DataView(CommentSentimentTable);
+    // Hide loading image once chart is drawn
+    document.getElementById('loading-img').style.display = 'none';
+
+    const view = new google.visualization.DataView(CommentSentimentTable);
     const chart = new google.visualization.ColumnChart(
         document.getElementById('chart-container'));
-    chart.draw(view, options);    
+    chart.draw(view, options);
 
     const averageContainer = document.getElementById('average-score-container');
-    averageContainer.innerHTML = "Average Sentiment Score: " + averageScore;
+    averageContainer.innerHTML = 'Average Sentiment Score: ' + averageScore;
   });
 }
- 
+
 function toTooltipString(userComments) {
-  return userComments.map(comment => userCommentAsString(comment)).join("<br>"); 
+  return userComments.map(comment => userCommentAsString(comment)).join("<br>");
 }
 
 function userCommentAsString(comment) {
   commentMagnitude = comment.magnitude;  
-  return comment.commentMsg + "<br> Magnitude Score: " + commentMagnitude;
+  return comment.commentMsg + '<br> Magnitude Score: ' + commentMagnitude;
 }
 
 function convertRangeToString(range) {
-  return range.inclusiveStart + " to " + range.exclusiveEnd;  
+  return range.inclusiveStart + ' to ' + range.exclusiveEnd;
 }
