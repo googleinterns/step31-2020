81364dc566b8e690e157b0deb523230c8240b0b0
UriHarding <UriHarding@google.com>
2020-07-22 16:00:41 +0000

    Created basic testing structure which iterates through tests in for loop

--- src/test/javascript/controllers/urlInputTest.js
+++ src/test/javascript/controllers/urlInputTest.js
@@ -12,18 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//import script
- var script = document.createElement('script');
- script.src = 'src/main/webapp/common_util.js'
+// Import script
+var script = document.createElement('script');
+script.src = 'src/main/webapp/common_util.js'
+var x = 0
 
-  describe('The application controller', function() {
-    it('should pass', function() {
-      expect(true).toBe(true);
-    });
+// Test Urls derived from https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486
+// These encompass the URL's that the parser is able to handle.
+var outputUrls = ["dQw4w9WgXcQ","-wtIMTCHWuI","-wtIMTCHWuI","JdfC0C9V6ZI"];
+var testUrls = ["youtube.com/watch?v=dQw4w9WgXcQ", "http://youtu.be/-wtIMTCHWuI",
+    "http://www.youtube.com/oembed?url=http%3A//www.youtube.com/watch?v%3D-wtIMTCHWuI",
+    "http://www.youtube.com/attribution_link?a=JdfC0C9V6ZI&u=%2Fwatch%3Fv%3DEhxJLojIE_o%26feature%3Dshare",
+    ]
 
-    it('takes in the simplest url form and returns the videoID', function() {
-      expect(extractYouTubeUrl("youtube.com/watch?v=dQw4w9WgXcQ")).toBe("dQw4w9WgXcQ");
+describe('The application controller', function() {
+  for(i = 0; i < testUrls.length; i++) {
+    it('ensures test url ' + i + ' returns expected url ' + i +'.', function() {
+      // Iterator i could not be passed into the array in the loop, so a secondary iterator had to be used.
+      expect(extractYouTubeUrl(testUrls[x])).toBe(outputUrls[x]);
+      x++;
     });
-  });
+  }
+});
 
 
