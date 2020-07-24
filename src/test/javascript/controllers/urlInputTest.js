9bd88a0474744745748304e31a45fb86261abd20
UriHarding <UriHarding@google.com>
2020-07-24 18:37:22 +0000

    resolving linter issues with URL input test

--- src/test/javascript/controllers/urlInputTest.js
+++ src/test/javascript/controllers/urlInputTest.js
@@ -15,31 +15,32 @@
 // Import script
 const script = document.createElement('script');
 script.src = 'src/main/webapp/common_util.js';
-var x = 0;
+let x = 0;
 
 // Test Urls sampled from https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486
 // These encompass the URL's that the parser is able to handle.
-const outputUrls = ['dQw4w9WgXcQ','-wtIMTCHWuI', 'yZv2daTWRZU', 'QdK8U-VIH_o',
-    '0zM3nApSvMg','0zM3nApSvMg','1p3vcRhsYGo', 'dQw4w9WgXcQ','dQw4w9WgXcQ',
-    'dQw4w9WgXcQ', '6L3ZvIMwZFM', 'oTJRivZTMLs', 'oTJRivZTMLs', null];
+const outputUrls = ['dQw4w9WgXcQ', '-wtIMTCHWuI', 'yZv2daTWRZU', 'QdK8U-VIH_o',
+  '0zM3nApSvMg', '0zM3nApSvMg', '1p3vcRhsYGo', 'dQw4w9WgXcQ', 'dQw4w9WgXcQ',
+  'dQw4w9WgXcQ', '6L3ZvIMwZFM', 'oTJRivZTMLs', 'oTJRivZTMLs', null];
 const testUrls = ['youtube.com/watch?v=dQw4w9WgXcQ', 'http://youtu.be/-wtIMTCHWuI',
-    'https://www.youtube.com/watch?v=yZv2daTWRZU&feature=em-uploademail',
-    'https://www.youtube.com/user/IngridMichaelsonVEVO#p/a/u/1/QdK8U-VIH_o',
-    'https://www.youtube.com/watch?v=0zM3nApSvMg#t=0m10s',
-    'https://www.youtube.com/embed/0zM3nApSvMg?rel=0',
-    'http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo',
-    'http://youtube.com/?vi=dQw4w9WgXcQ&feature=youtube_gdata_player',
-    'http://www.youtube.com/e/dQw4w9WgXcQ',
-    'http://www.youtube.com/watch?feature=player_embedded&v=dQw4w9WgXcQ',
-    'http://www.youtube-nocookie.com/v/6L3ZvIMwZFM?version=3&hl=en_US&rel=0',
-    'http://youtube.com/watch?vi=oTJRivZTMLs&feature=channel',
-    'http://www.youtube.com/user/dreamtheater#p/u/1/oTJRivZTMLs',
-    'Definitely_Not_a_URL'];
+  'https://www.youtube.com/watch?v=yZv2daTWRZU&feature=em-uploademail',
+  'https://www.youtube.com/user/IngridMichaelsonVEVO#p/a/u/1/QdK8U-VIH_o',
+  'https://www.youtube.com/watch?v=0zM3nApSvMg#t=0m10s',
+  'https://www.youtube.com/embed/0zM3nApSvMg?rel=0',
+  'http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo',
+  'http://youtube.com/?vi=dQw4w9WgXcQ&feature=youtube_gdata_player',
+  'http://www.youtube.com/e/dQw4w9WgXcQ',
+  'http://www.youtube.com/watch?feature=player_embedded&v=dQw4w9WgXcQ',
+  'http://www.youtube-nocookie.com/v/6L3ZvIMwZFM?version=3&hl=en_US&rel=0',
+  'http://youtube.com/watch?vi=oTJRivZTMLs&feature=channel',
+  'http://www.youtube.com/user/dreamtheater#p/u/1/oTJRivZTMLs',
+  'Definitely_Not_a_URL'];
 
 describe('The application controller', function() {
-  for(i = 0; i < testUrls.length; i++) {
+  for (i = 0; i < testUrls.length; i++) {
     it('ensures test url ' + i + ' returns expected url ' + i +'.', function() {
-      // Iterator i could not be passed into the array in the loop, so a secondary iterator had to be used.
+      // Iterator i could not be passed into the array in the loop,
+      // So a secondary iterator had to be used.
       expect(extractYouTubeUrl(testUrls[x])).toBe(outputUrls[x]);
       x++;
     });
