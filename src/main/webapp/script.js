d380ca8a51f212ad7a2c42451be48cd097ec2986
Xin <xh23@rice.edu>
2020-08-04 08:58:41 -0500

    changes for duration and js lint

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -115,8 +115,7 @@ function displaySentimentBucketChart(sentimentBucketList) {
     'height': CHART_HEIGHT,
     'bar': {groupWidth: '100'},
     'tooltip': {isHtml: true},
-    'animation': {"startup": true,
-                   "duration": 200},
+    'animation': {'startup': true, 'duration': 1000},
   };
 
   const view = new google.visualization.DataView(CommentSentimentTable);
